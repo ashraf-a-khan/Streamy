feae363e4a1234bd3903bbe4c69954e1ff278804
ashraf-a-khan <khan.ashraf2311@gmail.com>
2021-01-05 15:20:22 -0500

    Initialize library before oAuth process

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "1041851591836-fpscrtmlhh5p39sr1eedkha1g1ishkli.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
